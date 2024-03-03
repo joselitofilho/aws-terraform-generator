9850e7a17d8b39d2ec615703b5b1f9d75902ac0a
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2024-03-03 21:20:05 +0000

    tests: adds test cases for the generator

--- internal/generators/generators.go
+++ internal/generators/generators.go
@@ -18,13 +18,24 @@ import (
 
 var ErrUnsupportedFileType = errors.New("unsupported file type")
 
+// Build executes the provided templateContent using the data supplied and returns the resulting string.
+// The templateContent is parsed using the given templateName and executed with the provided data.
+// The data parameter can be of any type.
+//
+// Parameters:
+//   - data: The input data to be used for populating the template.
+//   - templateName: The name of the template. This could serve as an identifier.
+//   - templateContent: The content of the template in the Go text/template format.
+//
+// Returns:
+//   - string: The output generated from executing the template with the provided data.
+//   - error: An error, if any, encountered during parsing or execution of the template.
 func Build(data any, templateName, templateContent string) (string, error) {
 	tmpl, err := buildAndParseTemplate(templateName, templateContent)
 	if err != nil {
 		return "", fmt.Errorf("%w", err)
 	}
 
-	// Execute the template with the data and capture the output in a buffer.
 	var output bytes.Buffer
 
 	err = tmpl.Execute(&output, data)
@@ -35,6 +46,16 @@ func Build(data any, templateName, templateContent string) (string, error) {
 	return output.String(), nil
 }
 
+// CreateFilesMap takes a slice of config.File structs and creates a map where the keys are strings
+// and the values are File structs. Each element in the slice is used to populate the corresponding
+// key-value pair in the map.
+//
+// Parameters:
+//   - files: A slice of config.File structs representing the files to be mapped.
+//
+// Returns:
+//   - A map[string]File: A map containing the files where the keys are file names and
+//     the values are File structs containing the file's template and imports.
 func CreateFilesMap(files []config.File) map[string]File {
 	filesConf := map[string]File{}
 	for i := range files {
@@ -47,6 +68,33 @@ func CreateFilesMap(files []config.File) map[string]File {
 	return filesConf
 }
 
+// CreateTemplatesMap creates a map[string]string from a slice of config.FilenameTemplateMap.
+//
+// The function takes a slice of config.FilenameTemplateMap as input. Each element in the slice
+// represents a map where keys are filenames (strings) and values are corresponding templates (strings).
+// It iterates over each map in the slice and merges them into a single map[string]string where each
+// filename is mapped to its respective template.
+//
+// Parameters:
+//   - filenameTemplatesList: A slice of config.FilenameTemplateMap, where each element is a map
+//     representing filename-template pairs.
+//
+// Returns:
+//   - map[string]string: A map where keys are filenames and values are corresponding templates.
+//
+// Example:
+//
+//	filenameTemplatesList := []config.FilenameTemplateMap{
+//	    {"file1.txt": "template1"},
+//	    {"file2.txt": "template2"},
+//	    {"file3.txt": "template3"},
+//	}
+//	templatesMap := CreateTemplatesMap(filenameTemplatesList)
+//	fmt.Println(templatesMap)
+//
+// Output:
+//
+//	map[file1.txt:template1 file2.txt:template2 file3.txt:template3]
 func CreateTemplatesMap(filenameTemplatesList []config.FilenameTemplateMap) map[string]string {
 	templatesMap := map[string]string{}
 
@@ -59,6 +107,34 @@ func CreateTemplatesMap(filenameTemplatesList []config.FilenameTemplateMap) map[
 	return templatesMap
 }
 
+// FilterTemplatesMap filters a map of filenames to templates based on a given filter string.
+//
+// The function takes a filter string and a map of filenames to templates as input. It iterates
+// over each key-value pair in the input map and adds those pairs to a new map if the filename
+// contains the filter string. The function then returns the filtered map.
+//
+// Parameters:
+//   - filter: A string representing the filter condition. Only filenames containing this string
+//     will be included in the filtered map.
+//   - templatesMap: A map[string]string representing filenames mapped to their corresponding templates.
+//
+// Returns:
+//   - map[string]string: A map containing key-value pairs from the input map where the filenames
+//     contain the filter string.
+//
+// Example:
+//
+//	templatesMap := map[string]string{
+//	    "file1.txt": "template1",
+//	    "file2.txt": "template2",
+//	    "file3.txt": "template3",
+//	}
+//	filtered := FilterTemplatesMap("2", templatesMap)
+//	fmt.Println(filtered)
+//
+// Output:
+//
+//	map[file2.txt:template2 file3.txt:template3]
 func FilterTemplatesMap(filter string, templatesMap map[string]string) map[string]string {
 	filtred := map[string]string{}
 
@@ -71,6 +147,24 @@ func FilterTemplatesMap(filter string, templatesMap map[string]string) map[strin
 	return filtred
 }
 
+// GenerateFile generates a file using the provided template and data,
+// and writes the output to the specified outputFile. If fileTmpl is provided,
+// it uses that specific template; otherwise, it looks up the template based on
+// the fileName in the templatesMap. The generated file is then formatted based
+// on its extension. If an error occurs during the generation or formatting
+// process, it is returned. If a formatting error occurs and it is not due to
+// an unsupported file type, the error is printed to stdout, but the function
+// proceeds without returning an error.
+//
+// Parameters:
+//   - templatesMap: A map containing template strings where the keys are file names.
+//   - fileName: The name of the file.
+//   - fileTmpl: A specific template to use for generating the file (optional).
+//   - outputFile: The name of the output file to be generated.
+//   - data: The data to be used in the template for file generation.
+//
+// Returns:
+//   - An error, if any occurred during the generation or formatting process.
 func GenerateFile(templatesMap map[string]string, fileName, fileTmpl, outputFile string, data any) error {
 	var (
 		tmpl     string
@@ -97,6 +191,23 @@ func GenerateFile(templatesMap map[string]string, fileName, fileTmpl, outputFile
 	return err
 }
 
+// GenerateFiles generates multiple files using the provided templates and data,
+// and writes the outputs to the specified output directory. It iterates over
+// templates from the templatesMap and files from the filesMap, merging them
+// into a single map of templates. For each template, it generates a file,
+// applies formatting based on its extension, and saves it to the output directory.
+// Errors that occur during file generation or formatting are collected and returned
+// as a single error. If any errors occur, the function returns an error indicating
+// that errors occurred during file generation or formatting.
+//
+// Parameters:
+//   - templatesMap: A map containing template strings where the keys are file names.
+//   - filesMap: A map containing File objects where the keys are file names.
+//   - data: The data to be used in the templates for file generation.
+//   - output: The directory where the generated files will be saved.
+//
+// Returns:
+//   - An error if any occurred during file generation or formatting, otherwise returns nil.
 func GenerateFiles(templatesMap map[string]string, filesMap map[string]File, data any, output string) error {
 	mergedTemplates := map[string]string{}
 
@@ -130,7 +241,6 @@ func GenerateFiles(templatesMap map[string]string, filesMap map[string]File, dat
 	return nil
 }
 
-// buildAndParseTemplate Create a new template and parse the template content.
 func buildAndParseTemplate(name, content string) (*template.Template, error) {
 	tmpl, err := template.New(name).
 		Funcs(template.FuncMap{
@@ -158,7 +268,6 @@ func buildFile(data any, templateName, templateContent, outputPath string) error
 		return fmt.Errorf("%w", err)
 	}
 
-	// Execute the template with the data and write the output to a file.
 	output, err := os.Create(outputPath)
 	if err != nil {
 		return fmt.Errorf("%w", err)
--- internal/generators/generators_test.go
+++ internal/generators/generators_test.go
@@ -2,8 +2,11 @@ package generators
 
 import (
 	_ "embed"
+	"os"
+	"path"
 	"testing"
 
+	"github.com/joselitofilho/aws-terraform-generator/internal/generators/config"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,12 +18,125 @@ func TestBuild(t *testing.T) {
 	}
 
 	tests := []struct {
-		name      string
-		args      args
-		want      string
-		targetErr error
+		name           string
+		args           args
+		want           string
+		expectedErrMsg string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "valid case",
+			args: args{
+				data:            map[string]any{"Name": "John", "Age": 30},
+				templateName:    "example",
+				templateContent: "Name: {{.Name}}, Age: {{.Age}}",
+			},
+			want: "Name: John, Age: 30",
+		},
+		{
+			name: "getFileByName func",
+			args: args{
+				data: map[string]any{"Files": map[string]File{"lambda.go": {
+					Imports: []string{"context"},
+					Tmpl:    "package main",
+				}}},
+				templateName:    "example",
+				templateContent: `{{getFileByName $.Files "lambda.go"}}`,
+			},
+			want: "{package main [context]}",
+		},
+		{
+			name: "getFileImports func",
+			args: args{
+				data: map[string]any{"Files": map[string]File{"lambda.go": {
+					Imports: []string{"context"},
+				}}},
+				templateName:    "example",
+				templateContent: `{{ range getFileImports $.Files "lambda.go" }}"{{ . }}"{{end}}`,
+			},
+			want: `"context"`,
+		},
+		{
+			name: "ToCamel func",
+			args: args{
+				data:            map[string]any{"Name": "my-name"},
+				templateName:    "example",
+				templateContent: "Name: {{ToCamel .Name}}",
+			},
+			want: "Name: myName",
+		},
+		{
+			name: "ToKebab func",
+			args: args{
+				data:            map[string]any{"Name": "myName"},
+				templateName:    "example",
+				templateContent: "Name: {{ToKebab .Name}}",
+			},
+			want: "Name: my-name",
+		},
+		{
+			name: "ToLower func",
+			args: args{
+				data:            map[string]any{"Name": "MY-NAME"},
+				templateName:    "example",
+				templateContent: "Name: {{ToLower .Name}}",
+			},
+			want: "Name: my-name",
+		},
+		{
+			name: "ToPascal func",
+			args: args{
+				data:            map[string]any{"Name": "my-name"},
+				templateName:    "example",
+				templateContent: "Name: {{ToPascal .Name}}",
+			},
+			want: "Name: MyName",
+		},
+		{
+			name: "ToSpace func",
+			args: args{
+				data:            map[string]any{"Name": "my-name"},
+				templateName:    "example",
+				templateContent: "Name: {{ToSpace .Name}}",
+			},
+			want: "Name: my name",
+		},
+		{
+			name: "ToSnake func",
+			args: args{
+				data:            map[string]any{"Name": "my-name"},
+				templateName:    "example",
+				templateContent: "Name: {{ToSnake .Name}}",
+			},
+			want: "Name: my_name",
+		},
+		{
+			name: "ToUpper func",
+			args: args{
+				data:            map[string]any{"Name": "my-name"},
+				templateName:    "example",
+				templateContent: "Name: {{ToUpper .Name}}",
+			},
+			want: "Name: MY-NAME",
+		},
+		{
+			name: "missing field",
+			args: args{
+				data:            map[string]any{"Name": "John", "Age": 30},
+				templateName:    "invalid",
+				templateContent: "{{ .MissingField }}",
+			},
+			want: "<no value>",
+		},
+		{
+			name: "invalid template",
+			args: args{
+				data:            map[string]any{"Name": "John", "Age": 30},
+				templateName:    "invalid",
+				templateContent: "{{ InvalidFunction .Name }}",
+			},
+			want:           "",
+			expectedErrMsg: "template: invalid:1: function \"InvalidFunction\" not defined",
+		},
 	}
 
 	for i := range tests {
@@ -29,8 +145,363 @@ func TestBuild(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			got, err := Build(tc.args.data, tc.args.templateName, tc.args.templateContent)
 
-			require.ErrorIs(t, err, tc.targetErr)
+			if tc.expectedErrMsg == "" {
+				require.NoError(t, err)
+				require.Equal(t, tc.want, got)
+			} else {
+				require.Error(t, err)
+				require.Equal(t, tc.expectedErrMsg, err.Error())
+			}
+		})
+	}
+}
+
+func TestCreateFilesMap(t *testing.T) {
+	type args struct {
+		files []config.File
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want map[string]File
+	}{
+		{
+			name: "empty input",
+			args: args{
+				files: []config.File{},
+			},
+			want: map[string]File{},
+		},
+		{
+			name: "single file",
+			args: args{
+				files: []config.File{
+					{
+						Name:    "example.txt",
+						Tmpl:    "This is an example file.",
+						Imports: []string{},
+					},
+				},
+			},
+			want: map[string]File{
+				"example.txt": {
+					Tmpl:    "This is an example file.",
+					Imports: []string{},
+				},
+			},
+		},
+		{
+			name: "multiple files",
+			args: args{
+				files: []config.File{
+					{
+						Name:    "file1.txt",
+						Tmpl:    "Template for file 1",
+						Imports: []string{"package1", "package2"},
+					},
+					{
+						Name:    "file2.txt",
+						Tmpl:    "Template for file 2",
+						Imports: []string{"package3"},
+					},
+				},
+			},
+			want: map[string]File{
+				"file1.txt": {
+					Tmpl:    "Template for file 1",
+					Imports: []string{"package1", "package2"},
+				},
+				"file2.txt": {
+					Tmpl:    "Template for file 2",
+					Imports: []string{"package3"},
+				},
+			},
+		},
+	}
+
+	for i := range tests {
+		tc := tests[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			got := CreateFilesMap(tc.args.files)
+
 			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestCreateTemplatesMap(t *testing.T) {
+	type args struct {
+		filenameTemplatesList []config.FilenameTemplateMap
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name: "empty List",
+			args: args{
+				filenameTemplatesList: []config.FilenameTemplateMap{},
+			},
+			want: map[string]string{},
+		},
+		{
+			name: "single element",
+			args: args{
+				filenameTemplatesList: []config.FilenameTemplateMap{
+					{"file1.txt": "template1"},
+				},
+			},
+			want: map[string]string{"file1.txt": "template1"},
+		},
+		{
+			name: "multiple elements",
+			args: args{
+				filenameTemplatesList: []config.FilenameTemplateMap{
+					{"file1.txt": "template1"},
+					{"file2.txt": "template2"},
+					{"file3.txt": "template3"},
+				},
+			},
+			want: map[string]string{
+				"file1.txt": "template1",
+				"file2.txt": "template2",
+				"file3.txt": "template3",
+			},
+		},
+		{
+			name: "duplicate keys",
+			args: args{
+				filenameTemplatesList: []config.FilenameTemplateMap{
+					{"file1.txt": "template1"},
+					{"file1.txt": "template2"},
+				},
+			},
+			want: map[string]string{"file1.txt": "template2"},
+		},
+	}
+
+	for i := range tests {
+		tc := tests[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			got := CreateTemplatesMap(tc.args.filenameTemplatesList)
+
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestFilterTemplatesMap(t *testing.T) {
+	type args struct {
+		filter       string
+		templatesMap map[string]string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name: "empty Map",
+			args: args{
+				filter:       "test",
+				templatesMap: map[string]string{},
+			},
+			want: map[string]string{},
+		},
+		{
+			name: "no matches",
+			args: args{
+				filter: "xyz",
+				templatesMap: map[string]string{
+					"file1.txt": "template1",
+					"file2.txt": "template2",
+					"file3.txt": "template3",
+				},
+			},
+			want: map[string]string{},
+		},
+		{
+			name: "single match",
+			args: args{
+				filter: "2",
+				templatesMap: map[string]string{
+					"file1.txt": "template1",
+					"file2.txt": "template2",
+					"file3.txt": "template3",
+				},
+			},
+			want: map[string]string{"file2.txt": "template2"},
+		},
+		{
+			name: "multiple matches",
+			args: args{
+				filter: "file",
+				templatesMap: map[string]string{
+					"file1.txt": "template1",
+					"file2.txt": "template2",
+					"file3.txt": "template3",
+					"other.txt": "othertemplate",
+				},
+			},
+			want: map[string]string{
+				"file1.txt": "template1",
+				"file2.txt": "template2",
+				"file3.txt": "template3",
+			},
+		},
+	}
+
+	for i := range tests {
+		tc := tests[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			got := FilterTemplatesMap(tc.args.filter, tc.args.templatesMap)
+
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestGenerateFile(t *testing.T) {
+	type args struct {
+		templatesMap map[string]string
+		fileName     string
+		fileTmpl     string
+		outputFile   string
+		data         any
+	}
+
+	testOutput := "./testoutput"
+	_ = os.MkdirAll(testOutput, os.ModePerm)
+
+	tests := []struct {
+		name             string
+		args             args
+		extraValidations func(testing.TB, string, error)
+		targetErr        error
+	}{
+		{
+			name: "successful go file generation and formatting",
+			args: args{
+				templatesMap: map[string]string{"test.go": "type  My{{.Name}}Struct    struct   {}"},
+				fileName:     "test.go",
+				outputFile:   path.Join(testOutput, "output.go"),
+				data:         struct{ Name string }{Name: "World"},
+			},
+			extraValidations: func(tb testing.TB, outputFile string, err error) {
+				if err != nil {
+					return
+				}
+
+				data, err := os.ReadFile(outputFile)
+				require.NoError(tb, err)
+				require.Equal(tb, "type MyWorldStruct struct{}", string(data))
+			},
+		},
+		{
+			name: "successful tf file generation and formatting",
+			args: args{
+				templatesMap: map[string]string{"test.tf": `resource    "aws_s3_bucket"    "{{.Name}}_bucket"  {}`},
+				fileName:     "test.tf",
+				outputFile:   path.Join(testOutput, "output.tf"),
+				data:         struct{ Name string }{Name: "world"},
+			},
+			extraValidations: func(tb testing.TB, outputFile string, err error) {
+				if err != nil {
+					return
+				}
+
+				data, err := os.ReadFile(outputFile)
+				require.NoError(tb, err)
+				require.Equal(tb, `resource "aws_s3_bucket" "world_bucket" {}`, string(data))
+			},
+		},
+		{
+			name: "successful file generation without formatting for an unsuported ext",
+			args: args{
+				templatesMap: map[string]string{"test.txt": "Hello, {{.Name}}!"},
+				fileName:     "test.txt",
+				outputFile:   path.Join(testOutput, "output.txt"),
+				data:         struct{ Name string }{Name: "World"},
+			},
+			extraValidations: func(tb testing.TB, outputFile string, err error) {
+				if err != nil {
+					return
+				}
+
+				data, err := os.ReadFile(outputFile)
+				require.NoError(tb, err)
+				require.Equal(tb, "Hello, World!", string(data))
+			},
+		},
+	}
+
+	defer func() {
+		_ = os.RemoveAll(testOutput)
+	}()
+
+	for i := range tests {
+		tc := tests[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			err := GenerateFile(tc.args.templatesMap, tc.args.fileName, tc.args.fileTmpl, tc.args.outputFile, tc.args.data)
+
+			require.ErrorIs(t, err, tc.targetErr)
+
+			if tc.extraValidations != nil {
+				tc.extraValidations(t, tc.args.outputFile, err)
+			}
+		})
+	}
+}
+
+func TestGenerateFiles(t *testing.T) {
+	type args struct {
+		templatesMap map[string]string
+		filesMap     map[string]File
+		data         any
+		output       string
+	}
+
+	testOutput := "./testoutput"
+	_ = os.MkdirAll(testOutput, os.ModePerm)
+
+	tests := []struct {
+		name      string
+		args      args
+		targetErr error
+	}{
+		{
+			name: "generate single file",
+			args: args{
+				templatesMap: map[string]string{
+					"template.txt": "Hello, {{.Name}}!",
+				},
+				filesMap: nil,
+				data:     struct{ Name string }{"John"},
+				output:   testOutput,
+			},
+			targetErr: nil,
+		},
+	}
+
+	defer func() {
+		_ = os.RemoveAll(testOutput)
+	}()
+
+	for i := range tests {
+		tc := tests[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			err := GenerateFiles(tc.args.templatesMap, tc.args.filesMap, tc.args.data, tc.args.output)
+
+			require.ErrorIs(t, err, tc.targetErr)
 		})
 	}
 }
