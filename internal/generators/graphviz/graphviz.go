ee7e34acf62fd3e60dd1ae9bd7ae05b396d1fd64
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2024-03-06 23:40:59 +0000

    feat: implements the graphviz generator

---
+++ internal/generators/graphviz/graphviz.go
@@ -0,0 +1,28 @@
+package graphviz
+
+import (
+	"github.com/emicklei/dot"
+
+	"github.com/joselitofilho/aws-terraform-generator/internal/drawio"
+)
+
+func Build(resources *drawio.ResourceCollection, resourceImageMap map[drawio.ResourceType]string) (string, error) {
+	g := dot.NewGraph(dot.Directed)
+
+	g.NodeInitializer(func(n dot.Node) {
+		n.Attrs("shape", "plaintext", "imagepos", "tc", "labelloc", "b", "height", "0.9")
+	})
+
+	nodes := map[string]dot.Node{}
+
+	for _, res := range resources.Resources {
+		nodes[res.ID()] = g.Node(res.Value()).
+			Attr("image", resourceImageMap[res.ResourceType()])
+	}
+
+	for _, rel := range resources.Relationships {
+		g.Edge(nodes[rel.Source.ID()], nodes[rel.Target.ID()])
+	}
+
+	return g.String(), nil
+}
