b33a0d169d08b05747fe7bf8ee2fd3cbb1834ab3
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2024-03-09 11:20:05 +0000

    tests: resources

--- internal/resources/resource_type_enum_test.go
+++ internal/resources/resource_type_enum_test.go
@@ -12,16 +12,17 @@ func TestResourceType_String(t *testing.T) {
 		rt   ResourceType
 		want string
 	}{
+		{name: "APIGateway", rt: APIGatewayType, want: "APIGateway"},
+		{name: "Cron", rt: CronType, want: "Cron"},
+		{name: "Database", rt: DatabaseType, want: "Database"},
+		{name: "Endpoint", rt: EndpointType, want: "Endpoint"},
+		{name: "GoogleBQ", rt: GoogleBQType, want: "GoogleBQ"},
+		{name: "Kinesis", rt: KinesisType, want: "Kinesis"},
 		{name: "Lambda", rt: LambdaType, want: "Lambda"},
+		{name: "RestfulAPI", rt: RestfulAPIType, want: "RestfulAPI"},
+		{name: "S3", rt: S3Type, want: "S3"},
 		{name: "SQS", rt: SQSType, want: "SQS"},
 		{name: "SNS", rt: SNSType, want: "SNS"},
-		{name: "Cron", rt: CronType, want: "Cron"},
-		{name: "APIGateway", rt: APIGatewayType, want: "APIGateway"},
-		{name: "Endpoint", rt: EndpointType, want: "Endpoint"},
-		{name: "S3", rt: S3Type, want: "S3"},
-		{name: "Database", rt: DatabaseType, want: "Database"},
-		{name: "RestfulAPI", rt: RestfulAPIType, want: "RestfulAPI"},
-		{name: "Kinesis", rt: KinesisType, want: "Kinesis"},
 		{name: "Unknown", rt: "", want: "Unknown"},
 	}
 
@@ -42,16 +43,17 @@ func TestParseResourceType(t *testing.T) {
 		input  string
 		output ResourceType
 	}{
+		{name: "Parse APIGateway", input: "APIGateway", output: APIGatewayType},
+		{name: "Parse Cron", input: "Cron", output: CronType},
+		{name: "Parse Database", input: "Database", output: DatabaseType},
+		{name: "Parse Endpoint", input: "Endpoint", output: EndpointType},
+		{name: "Parse GoogleBQ", input: "GoogleBQ", output: GoogleBQType},
+		{name: "Parse Kinesis", input: "Kinesis", output: KinesisType},
 		{name: "Parse Lambda", input: "Lambda", output: LambdaType},
+		{name: "Parse RestfulAPI", input: "RestfulAPI", output: RestfulAPIType},
+		{name: "Parse S3", input: "S3", output: S3Type},
 		{name: "Parse SQS", input: "SQS", output: SQSType},
 		{name: "Parse SNS", input: "SNS", output: SNSType},
-		{name: "Parse Cron", input: "Cron", output: CronType},
-		{name: "Parse APIGateway", input: "APIGateway", output: APIGatewayType},
-		{name: "Parse Endpoint", input: "Endpoint", output: EndpointType},
-		{name: "Parse S3", input: "S3", output: S3Type},
-		{name: "Parse Database", input: "Database", output: DatabaseType},
-		{name: "Parse RestfulAPI", input: "RestfulAPI", output: RestfulAPIType},
-		{name: "Parse Kinesis", input: "Kinesis", output: KinesisType},
 		{name: "Parse Unknown", input: "Unknown", output: UnknownType},
 		{name: "Parse lowercase", input: "sqs", output: SQSType},
 		{name: "Parse uppercase", input: "SNS", output: SNSType},
